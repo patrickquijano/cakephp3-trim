6b5e59101b2bc3ee19c01a980a025cb14dbbb732
patrickquijano <patrickjosephquijano@gmail.com>
2020-01-02 18:20:33 +0800

    Arranged uses

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,8 @@
 <?php
 
+use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
-use Cake\Routing\Route\DashedRoute;
 
 Router::plugin('Trim', ['path' => '/trim'], function (RouteBuilder $routes) {
     $routes->fallbacks(DashedRoute::class);
